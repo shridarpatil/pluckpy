9564a83edb90b524919138ca66279fa89e2e8c26
shri <sp@routo.co>
2017-02-28 15:22:35 +0530

    pluck py

---
+++ pluck/pluck.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# Filename: pluck.py
+
+def pluck(dictionary, key):
+	try:
+		ages = [li[key] for li in dictionary]
+		return ages
+	except KeyError, e:
+		raise KeyError('Key not found')
+	except TypeError, e:
+		raise TypeError('dictionary must be of type Array')
+	
+version = '0.1'
+
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pluckpy',
+  packages = ['pluckpy'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A random test lib',
+  author = 'Shridhar Patil',
+  author_email = 'shridharpatil2792@gmail.com',
+  url = 'https://github.com/shridarpatil/pluckpy', #URL to the github repo
+  download_url = 'https://github.com/shridarpatil/underscorepy/archive/0.1.tar.gz',
+  keywords = ['pluck', 'pluckpy'], # arbitrary keywords
+  classifiers = [],
+)
