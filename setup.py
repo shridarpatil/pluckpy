1ed3bdc8e774ba80a8b0c6cf9ace8fc46ce80f6f
shridarpatil <sp@routo.co>
2017-02-28 15:48:22 +0530

    new release

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'pluckpy',
   packages = ['pluckpy'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.1.0',
   description = 'A random test lib',
   author = 'Shridhar Patil',
   author_email = 'shridharpatil2792@gmail.com',
   url = 'https://github.com/shridarpatil/pluckpy', #URL to the github repo
-  download_url = 'https://github.com/shridarpatil/pluckpy/archive/0.1.tar.gz',
+  download_url = 'https://github.com/shridarpatil/pluckpy/archive/0.1.0.tar.gz',
   keywords = ['pluck', 'pluckpy'], # arbitrary keywords
   classifiers = [],
 )
